d63561a4804c309a5456acfffed4a598fa5260a2
JG-11 <jg08950@gmail.com>
2019-12-08 21:45:11 +0000

    Data from DB passed from DashboardController to dashboard view

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,8 +1,20 @@
 <?php
 namespace app\controllers;
 
+use app\models\{ SensorOne, SensorTwo, SensorThree };
+
 class DashboardController extends BaseController {
-    public function dashboardAction(){
-        return $this->renderHTML('dashboard.twig');
+    public function dashboardAction(){ 
+        $limit = 5;
+
+        $measurementsSensorOne = SensorOne::latest()->take($limit)->get();
+        $measurementsSensorTwo = SensorTwo::latest()->take($limit)->get();
+        $measurementsSensorThree = SensorThree::latest()->take($limit)->get();
+
+        return $this->renderHTML('dashboard.twig', [
+            'measurementsSensorOne' => $measurementsSensorOne,
+            'measurementsSensorTwo' => $measurementsSensorTwo,
+            'measurementsSensorThree' => $measurementsSensorThree
+        ]);
     }
 }
